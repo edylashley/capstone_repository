d99b417461b1e80a8d5c148e6c433ffb195387dc
Edyl Ashley <edylashley23@gmail.com>
2026-04-17 19:14:39 +0800

    added system logo in browser tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <link rel="icon" type="image/jpg" href="{{ asset('images/system-logo.jpg') }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <!-- Sidebar offset styles -->
         <style>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
 
+        <link rel="icon" type="image/jpg" href="{{ asset('images/system-logo.jpg') }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
