b17ce6d3559a3e8f729d64c2c9a8e330c5d698b8
Edyl Ashley <edylashley23@gmail.com>
2026-05-02 01:32:54 +0800

    hala syaa

--- resources/views/emails/support-resolved.blade.php
+++ resources/views/emails/support-resolved.blade.php
@@ -1,17 +1,20 @@
 <x-mail::message>
-# Support Request Resolved
+    # Support Request Resolved!
 
-Your support request regarding **"{{ $ticket->subject }}"** has been marked as resolved by the administrative team.
+    Your support request regarding **"{{ $ticket->subject }}"** has been marked as resolved by the administrative team.
 
-@if($ticket->admin_reply)
-<x-mail::panel>
-<div style="color: #38bdf8; font-weight: 800; text-transform: uppercase; font-size: 11px; letter-spacing: 1px; margin-bottom: 5px;">Administrator Resolution Notes</div>
-<div style="color: #f1f5f9;">{{ $ticket->admin_reply }}</div>
-</x-mail::panel>
-@endif
+    @if($ticket->admin_reply)
+        <x-mail::panel>
+            <div
+                style="color: #38bdf8; font-weight: 800; text-transform: uppercase; font-size: 11px; letter-spacing: 1px; margin-bottom: 5px;">
+                Administrator Resolution Notes</div>
+            <div style="color: #f1f5f9;">{{ $ticket->admin_reply }}</div>
+        </x-mail::panel>
+    @endif
 
-If you believe this ticket was closed in error or have further questions, please submit a new request through the portal.
+    If you believe this ticket was closed in error or have further questions, please submit a new request through the
+    portal.
 
-Regards,<br>
-**{{ config('app.name') }} Support Team**
+    Regards,<br>
+    **{{ config('app.name') }} Team**
 </x-mail::message>
